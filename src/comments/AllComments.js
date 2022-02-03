6851e2cf7d70b9a0701a1ff9fbdacc0413b76ccf
MJalalkhan <jalal.khan125@gmail.com>
2022-02-03 23:04:10 +0500

    sign in and register

---
+++ src/comments/AllComments.js
@@ -0,0 +1,31 @@
+import { Divider, Avatar, Grid, Paper } from "@material-ui/core";
+
+const imgLink =
+  "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
+
+export const AllComments = () => {
+  return (
+    <div style={{ padding: 14 }} className="App">
+      <h1>Comments</h1>
+      <Paper style={{ padding: "40px 20px" }}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Avatar alt="Remy Sharp" src={imgLink} />
+          </Grid>
+          <Grid justifyContent="left" item xs zeroMinWidth>
+            <h4 style={{ margin: 0, textAlign: "left" }}>Michel Michel</h4>
+            <p style={{ textAlign: "left" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              luctus ut est sed faucibus. Duis bibendum ac ex vehicula laoreet.
+              Suspendisse congue vulputate lobortis.{" "}
+            </p>
+            <p style={{ textAlign: "left", color: "gray" }}>
+              posted 1 minute ago
+            </p>
+          </Grid>
+        </Grid>
+        <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
+      </Paper>
+    </div>
+  );
+};
